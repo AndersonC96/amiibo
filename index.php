16e70f82607e71b284b2d4b53fec54c8ad05714c
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-07 20:36:09 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Amiibo</title>
+        <link rel="shortcut icon" href="favicon.ico">
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <script src="js/constants.js"></script>
+        <script src="js/lib/wrapper.js"></script>
+        <script src="js/index/view.js"></script>
+        <script src="js/index/main.js" defer></script>
+        <link rel="stylesheet" href="css/index/main.css">
+    </head>
+    <body>
+        <?php
+            include_once "includes/header.php";
+        ?>
+        <main>
+            <h2 id="amiibo-list-title">Todos os amiibos</h2>
+            <hr>
+            <div id="loading-img"></div>
+            <section id="amiibo-list"></section>
+            <div id="bottom-gradient"></div>
+        </main>
+        <?php
+            include_once "includes/hamburger.php";
+        ?>
+    </body>
+</html>
