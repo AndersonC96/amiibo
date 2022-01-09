f42dbdc8cc2d55cef57e8178354e90c5e8e1b51a
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-09 15:59:49 -0300

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+const AMIIBO_URL_ENDPOINT = "https://amiiboapi.com/api/amiibo";
+const ID_PREFIX = "amiibo-";// prefixo para id
+const SCROLL_TOLERANCE = 200;// tolerancia para scroll
+const AMOUNT_PER_PAGE = 16;// quantidade de amiibos por pagina
+const REGIONS = {// regioes
+    au: "Austrália",
+    eu: "Europa",
+    jp: "Japão",
+    na: "América do Norte"
+};
