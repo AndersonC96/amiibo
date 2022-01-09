2214b98e4c0d5bd160eb448bcaad6d5a98f91e58
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-09 13:44:21 -0300

    Create hamburger.php

---
+++ includes/hamburger.php
@@ -0,0 +1,26 @@
+<div id="hamburger-menu">
+    <div>
+        <a href="index.php"><img src="img/icons/home_icon.svg" alt="Página inicial">&nbsp;Página inicial</a>
+    </div>
+    <hr>
+    <div>
+        <a href="amiibo.php?random"><img src="img/icons/random_icon.svg" alt="Amiibo aleatório">&nbsp;Amiibo aleatório</a>
+    </div>
+    <hr>
+    <div>
+        <a href="about.php"><img src="img/icons/about_icon.svg" alt="Sobre">&nbsp;Sobre</a>
+    </div>
+    <div class="bottom">
+        <div id="close-menu-link">
+            <img src="img/icons/cross_icon.svg" alt="Fechar">
+        </div>
+        <div id="change-hand-mode">
+            <span>Modo canhoto</span>
+            <img src="img/icons/left_hand_icon.svg" alt="Mão esquerda">
+        </div>
+    </div>
+</div>
+<div id="hamburger-btn" class="left">
+    <img src="img/icons/hamburger_icon.svg" alt="Menu">
+</div>
+<script src="js/global/hamburger.js"></script>
