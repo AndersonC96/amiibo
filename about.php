9eb07b9e74d6c40eca2a00361f2a8dd490eb9e5e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-09 15:56:41 -0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Amiibo</title>
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="stylesheet" href="css/about/main.css">
+    </head>
+    <body>
+        <?php
+            include_once "includes/header.php";
+        ?>
+        <main>
+            <h1>Sobre</h1>
+            <p>
+
+            </p>
+            <p>
+                
+            </p>
+            <p>
+                
+            </p>
+            <p>
+                
+            </p>
+            <hr>
+            <p class="signature">Anderson</p>
+        </main>
+        <?php
+            include_once "includes/hamburger.php";
+        ?>
+    </body>
+</html>
