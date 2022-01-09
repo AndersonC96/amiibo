137ae56ed0e54e2fd21eee2a998f1618fa579ea5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-09 16:37:06 -0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -3,8 +3,8 @@ const ID_PREFIX = "amiibo-";// prefixo para id
 const SCROLL_TOLERANCE = 200;// tolerancia para scroll
 const AMOUNT_PER_PAGE = 16;// quantidade de amiibos por pagina
 const REGIONS = {// regioes
+    na: "América do Norte",
     au: "Austrália",
     eu: "Europa",
-    jp: "Japão",
-    na: "América do Norte"
+    jp: "Japão"
 };
